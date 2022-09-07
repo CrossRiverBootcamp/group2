cf08c22c7f77ddefde8850391ffbd26da76875e3
Esther_Weller <sstt1414@gmail.com>
2022-09-07 15:29:21 +0300

    add Transaction.NSB

---
+++ EandLBankSystem/Transaction.NSB/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using NServiceBus.Logging;
+using Transaction.Service;
+
+class Program
+{
+    static ILog log = LogManager.GetLogger<Program>();
+
+    static async Task Main()
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings.json");
+
+        var config = configuration.Build();
+        var databaseConnection = config.GetConnectionString("TransactionDatabase");
+        var queueName = config.GetSection("Queues:TransactionQueue:Name").Value;
+
+        Console.Title = queueName;
+        var endpointConfiguration = new EndpointConfiguration(queueName);
+
+        #region Receiver Configuration
+
+        var rabbitMQConnection = config.GetConnectionString("RabbitMQ");
+        var NSBConnection = config.GetConnectionString("NSBConnection");
+
+        endpointConfiguration.EnableInstallers();
+        endpointConfiguration.EnableOutbox();
+
+        var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+        persistence.ConnectionBuilder(
+            connectionBuilder: () =>
+            {
+                return new SqlConnection(NSBConnection);
+            });
+
+        var dialect = persistence.SqlDialect<SqlDialect.MsSqlServer>();
+
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.ConnectionString(rabbitMQConnection);
+        transport.UseConventionalRoutingTopology(QueueType.Quorum);
+
+        //var conventions = endpointConfiguration.Conventions();
+        //conventions.DefiningEventsAs(type => type.Namespace == "Subscriber.Messages.Events");
+
+        #endregion
+
+        var containerSettings = endpointConfiguration.UseContainer(new DefaultServiceProviderFactory());
+        containerSettings.ServiceCollection.AddServicesExtention(databaseConnection);
+        containerSettings.ServiceCollection.AddScoped<ITransactionService, TransactionService>();
+        containerSettings.ServiceCollection.AddAutoMapper(typeof(Program));
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+        Console.WriteLine("waiting for messages...");
+        Console.ReadLine();
+
+        await endpointInstance.Stop();
+    }
+}
---
+++ EandLBankSystem/Transaction.NSB/TransactionPolicyData.cs
@@ -0,0 +1,10 @@
+﻿using NServiceBus;
+
+namespace Transaction.NSB;
+
+public class TransactionPolicyData : ContainSagaData
+{
+    public int TransactionId { get; set; }
+    public bool SagaTransactionStarted { get; set; }
+    public bool SagaTransactionEnded { get; set; }
+}
