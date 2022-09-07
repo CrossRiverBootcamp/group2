66dcbcdc7efb0f80a1a187db30cb59b0db1f0644
Esther_Weller <sstt1414@gmail.com>
2022-09-07 15:28:54 +0300

    add Transaction.Messages

---
+++ EandLBankSystem/Transaction.Messages/Commands/StartTransactionSaga.cs
@@ -0,0 +1,16 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction.Messages.Commands;
+
+public class StartTransactionSaga: ICommand
+{
+    public int TransactionId { get; set; }
+    public int FromAccount { get; set; }
+    public int ToAccount { get; set; }
+    public int Ammount { get; set; }
+}
---
+++ EandLBankSystem/Transaction.Messages/Events/TransactionSagaStarted.cs
@@ -0,0 +1,16 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction.Messages.Events;
+
+public class TransactionSagaStarted:IEvent
+{
+    public int TransactionId { get; set; }
+    public int FromAccount { get; set; }
+    public int ToAccount { get; set; }
+    public int Ammount { get; set; }
+}
