ba2fd7431d287e05f5b4dc5db4f2b62c44bde6a6
Esther_Weller <sstt1414@gmail.com>
2022-09-09 12:33:14 +0300

    fix User service: client

--- my-angular-app/src/app/services/user.service.ts
+++ my-angular-app/src/app/services/user.service.ts
@@ -1,54 +1,37 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Account } from '../models/Account';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private user?: Account;
-  private authorized: boolean = false;
-  private accountID?: number;
-  private authoBehavior = new BehaviorSubject<boolean>(false);
-  private auth = this.authoBehavior.asObservable();
-  constructor() {}
+  private static accountID?: number = Number(
+    sessionStorage.getItem('accountID')
+  );
+  private static authoBehavior = new BehaviorSubject<boolean>(false);
+  private static auth = UserService.authoBehavior.asObservable();
 
   getAuth(): Observable<boolean> {
-    return this.auth;
+    if (UserService.accountID) this.setAuth(true);
+    else this.setAuth(false);
+    return UserService.auth;
   }
 
   setAuth(bool: boolean) {
-    this.authoBehavior.next(bool);
-  }
-
-  setUser(user: Account) {
-    this.user = user;
-    this.authorized = true;
-    this.setAuth(true);
-  }
-
-  getUser() {
-    return this.user;
+    UserService.authoBehavior.next(bool);
   }
 
   setAccountID(id: number) {
-    this.authorized = true;
     this.setAuth(true);
-    this.accountID = id;
+    sessionStorage.setItem('accountID', id.toString());
   }
 
   getAccountID(): number | undefined {
-    return this.accountID;
-  }
-
-  isAuthorized(): boolean {
-    return this.authorized;
+    return UserService.accountID;
   }
 
   logOut() {
-    this.user = undefined;
-    this.authorized = false;
-    this.accountID = undefined;
+    sessionStorage.clear();
     this.setAuth(false);
   }
 }
